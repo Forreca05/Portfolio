98e974aff2ccca26596d8ed5d5ab051108ef8e7f
João Ferreira <joaopedronf6b@gmail.com>
2026-02-04 11:39:20 +0000

    Hero section using motion

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -1,24 +1,96 @@
+import { FlipWords } from "./FlipWords"
+import {motion} from "motion/react"
+
 const HeroText = () => {
+  const words = ["Every day", "Every Week", "Every Month"];
+  const variants = {
+    hidden: { opacity: 0, x: -50 },
+    visible: { opacity: 1, x: 0 }
+  }
   return (
     <div className="z-10 mt-20 text-center md:mt-40
     md:text-left rounded-3xl bg-clip-text">
         {/* Desktop View */}
         <div className="flex-col hidden md:flex c-space">
-            <h1 className="text-4xl font-medium">Hi I'm João</h1>
+            <motion.h1 
+                className="text-4xl font-medium"
+                variants={variants}
+                initial="hidden"
+                animate="visible"
+                transition={{delay:1}}>
+                Hi I'm João
+            </motion.h1>
             <div className="flex flex-col items-start">
-                <p className="text-5xl font-medium 
-                text-neutral-300">
+                <motion.p 
+                    className="text-5xl font-medium 
+                    text-neutral-300"
+                    variants={variants}
+                    initial="hidden"
+                    animate="visible"
+                    transition={{delay:1.2}}>
                     An Informatics Enginner Student <br />
                     Eager to Grow
-                </p>
-                <div></div>
-                <p className="text-4xl font-medium
-                text-neutral-300">
+                </motion.p>
+                <motion.div
+                    variants={variants}
+                    initial="hidden"
+                    animate="visible"
+                    transition={{delay:1.5}}
+                >
+                    <FlipWords words={words} 
+                        className="font-black text-white text-8xl"/>
+                </motion.div>
+                <motion.p 
+                    className="text-4xl font-medium
+                    text-neutral-300"
+                    variants={variants}
+                    initial="hidden"
+                    animate="visible"
+                    transition={{delay:1.8}}>
                     And Learn Even More
-                </p>
+                </motion.p>
             </div>
         </div>
-        
+        {/* Mobile View */}
+        <div className="flex flex-col space-y-6 md:hidden">
+            <motion.p 
+                className="text-4xl font-medium"
+                variants={variants}
+                initial="hidden"
+                animate="visible"
+                transition={{delay:1}}>
+                Hi, I'm João
+            </motion.p>
+            <div>
+                <motion.p 
+                    className="text-5xl font-black
+                    text-neutral-300"
+                    variants={variants}
+                    initial="hidden"
+                    animate="visible"
+                    transition={{delay:1.2}}>
+                    Studying
+                </motion.p>
+                <motion.div
+                    variants={variants}
+                    initial="hidden"
+                    animate="visible"
+                    transition={{delay:1.5}}
+                >
+                    <FlipWords words={words} 
+                        className="font-bold text-white text-7xl"/>
+                </motion.div>
+                <motion.p 
+                    className="text-4xl font-black 
+                    text-neutral-300"
+                    variants={variants}
+                    initial="hidden"
+                    animate="visible"
+                    transition={{delay:1.8}}>
+                    To Become Even Better
+                </motion.p>
+            </div>
+        </div>
     </div>
   )
 }
