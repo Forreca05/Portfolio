0ecbf22815f678abd1f438893a4e64daffd735ae
João Ferreira <joaopedronf6b@gmail.com>
2026-02-05 15:23:43 +0000

    Initiated About Me section

---
+++ src/sections/About.jsx
@@ -0,0 +1,106 @@
+import { useRef } from "react";
+import Card from "../components/Card"
+import { Globe } from "../components/Globe";
+
+const About = () => {
+    const grid2Container = useRef()
+    return (
+        <section className="c-space section-spacing">
+            <h2 className="text-heading">About Me</h2>
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-6
+        md:auto-rows-[18rem] mt-12">
+                {/* Grid 1 */}
+                <div className="flex items-end grid-default-color grid-1">
+                    <img
+                        src="assets/coding-pov.png"
+                        className="absolute scale-[1.75] -right-20 -top-4 
+                    md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]"/>
+                    <div className="z-10">
+                        <p className="headtext">Hi, I'm João Ferreira</p>
+                        <p className="subtext">
+                            I'm a 3rd Year Informatics Engineer Student
+                            at FEUP.
+                            Over the last 3 years i've developed a strong
+                            passion for this field, specially on
+                            Low Level and Systems Programming.
+                        </p>
+                        <div className="absolute inset-x-0 pointer-evets-none 
+                        -bottom-4 h-1/2 sm:h-1/3 from-indigo" />
+                    </div>
+                </div>
+                {/* Grid 2 */}
+                <div className="grid-default-color grid-2">
+                    <div ref= {grid2Container} className="flex items-center 
+                    justify-center w-full h-full">
+                        <p className="flex items-end 
+                        text-5xl text-gray-500">
+                            CODE IS MAGIC
+                        </p>
+                        <Card 
+                            style={{rotate: "45deg", top: "40%", left: "35%"}}
+                            text="STRUCTURE"
+                            containerRef={grid2Container}
+                        />
+                        <Card 
+                            style={{rotate: "25deg", top: "15%", left: "65%"}}
+                            text="INNOVATION"
+                            containerRef={grid2Container}
+                        />
+                        <Card 
+                            style={{rotate: "25deg", top: "65%", left: "5%"}}
+                            text="DISCIPLINE"
+                            containerRef={grid2Container}
+                        />
+                        <Card 
+                            style={{rotate: "-25deg", top: "65%", left: "65%"}}
+                            text="POWER"
+                            containerRef={grid2Container}
+                        />
+                        <Card 
+                            style={{rotate: "-25deg", top: "15%", left: "5%"}}
+                            text="ART"
+                            containerRef={grid2Container}
+                        />
+                        <Card 
+                            style={{rotate: "-25deg", top: "15%", left: "5%"}}
+                            image = "assets/logos/cplusplus.svg"
+                            containerRef={grid2Container}
+                        />
+                        <Card 
+                            style={{rotate: "-25deg", top: "65%", left: "65%"}}
+                            image = "assets/logos/javascript.svg"
+                            containerRef={grid2Container}
+                        />
+                        <Card 
+                            style={{rotate: "-34deg", top: "25%", left: "55%"}}
+                            image = "assets/logos/react.svg"
+                            containerRef={grid2Container}
+                        />
+                        <Card 
+                            style={{rotate: "-25deg", top: "75%", left: "5%"}}
+                            image = "assets/logos/visualstudiocode.svg"
+                            containerRef={grid2Container}
+                        />
+                    </div>
+                </div>
+                {/* Grid 3 */}
+                <div className="grid-black-color grid-3">
+                    <div className="z-10 w-[50%]">
+                        <p className="headtext">Time Zone</p>
+                        <p className="subtext">I'm based in Portugal but 
+                           open to remote work opportunities worldwide.</p>
+                    </div>
+                    <figure className="absolute left-[30%] top-[10%]">
+                        <Globe />
+                    </figure>
+                </div>
+                {/* Grid 4 */}
+                <div className="grid-special-color grid-4"></div>
+                {/* Grid 5 */}
+                <div className="grid-default-color grid-5"></div>
+            </div>
+        </section>
+    )
+}
+
+export default About
