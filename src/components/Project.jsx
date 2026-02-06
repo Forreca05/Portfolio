a8c03a19400bb55a1f13e2298ca7f55a01d878ed
João Ferreira <joaopedronf6b@gmail.com>
2026-02-06 16:25:35 +0000

    Added read more

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,23 +1,36 @@
 import React from 'react'
+import ProjectDetails from './ProjectDetails'
 
-const Project = () => {
+const Project = ({title, description, subDescription, href, image, tags}) => {
   return (
-    <div className="flex-wrap items-center py-10 justify-between
-    space-y-14 sm:flex sm:space-y-0">
-        <div>
-            <p className="text-2xl">title</p>
-            <div className="flex gap-5 mt-2 text-sand">
-                <span>tag1</span>
-                <span>tag2</span>
-                <span>tag3</span>
+    <>
+        <div className="flex-wrap items-center py-10 justify-between
+        space-y-14 sm:flex sm:space-y-0">
+            <div>
+                <p className="text-2xl">{title}</p>
+                <div className="flex gap-5 mt-2 text-sand">
+                    {tags.map((tag) => (
+                        <span key={tag.id}>{tag.name}</span>
+                    ))}
+                </div>
             </div>
+            <button className="flex items-center gap-1 cursor-pointer
+            hover-animation">
+                Read More
+                <img src="assets/arrow-right.svg" className="w-5" />
+            </button>
         </div>
-        <button className="flex items-center gap-1 cursor-pointer
-        hover-animation">
-            Read More
-            <img src="assets/arrow-right.svg" className="w-5" />
-        </button>
-    </div>
+        <div className="bg-gradient-to-r from-transparent
+        via-neutral-700 to-transparent h-px w-full"/>
+        <ProjectDetails 
+            title={title}
+            description={description}
+            subDescription={subDescription}
+            href={href}
+            image={image}
+            tags={tags}
+        />
+    </>  
   )
 }
 
---
+++ src/components/ProjectDetails.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const ProjectDetails = ({title, description, subDescription, href, image, tags }) => {
+  return (
+    <div className="fixed inset-0 z-50 flex items-center
+    justify-center w-full h-full overflow-hidden
+    backdrop-blur-sm">
+        <div className="relative nax-w-2xl border shadow-sm
+        rounded-2xl bg-gradient-to-l from-midnight to-navy
+        border-white/10">
+            <button className="absolute p-2 rounded-sm top-5 right-5
+            bg-midnight hover:bg-gray-500">
+                <img src="assets/close.svg" className="w-6 h-6" />
+            </button>
+            <img src={image} alt={title} className="w-full
+            rounded-t-2xl" />
+            <div>
+                <h5>{title}</h5>
+                <p>{description}</p>
+                {subDescription.map((sub, index) => (
+                    <p key={index}>{sub}</p>
+                ))}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ProjectDetails
